38ab4e0d10f7a46df6861cba424cc11f794f4084
josue <josue.ferrufino.soft@gmail.com>
2019-10-31 12:20:12 -0400

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Program program = new Program();
+            // stack move
+            //int[] MyStack ;
+            //int[] lLista = new int[] { 3, 8, 9, 7, 6 };
+            //MyStack = program.solution(lLista, 0);
+            //foreach (var item in MyStack)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+
+        }
+
+
+
+
+//A small frog wants to get to the other side of the road.The frog is currently located at position X and wants to get to a position greater than or equal to Y.The small frog always jumps a fixed distance, D.
+//Count the minimal number of jumps that the small frog must perform to reach its target.
+        public int solution(int X, int Y, int D) {
+
+
+            return 0;
+        }
+
+        // stack move 
+        //public int[] solution(int[] A,int k)
+        //{
+        //    if (A.Length > 0)
+        //    {
+        //        Stack<int> MyStack = new Stack<int>();
+        //        MyStack = ConvertStack(A);
+
+        //        for (int i = 0; i < k; i++)
+        //        {
+        //            int lastItem = MyStack.Pop();
+        //            MyStack = ConvertStack(MyStack.ToArray());
+        //            MyStack.Push(lastItem);
+        //            MyStack = ConvertStack(MyStack.ToArray());
+        //        }
+        //        MyStack = ConvertStack(MyStack.ToArray());
+        //        return MyStack.ToArray();
+        //    }
+        //    else {
+        //        return A;
+        //    }
+        //}
+
+
+        //public Stack<int> ConvertStack(int[] A) {
+        //    Stack<int> myStack = new Stack<int>();
+        //    for (int i = 0; i < A.Length; i++)
+        //    {
+        //        myStack.Push(A[i]);
+        //    }
+        //    return myStack;
+        //}
+    }
+}
